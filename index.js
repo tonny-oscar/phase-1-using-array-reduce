cacf6d90ac51626460595efb119c333c9dd0ea97
Tonny Bett <betttonny26@gmail.com>
2024-07-21 01:52:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+const totalBatteries = batteryBatches.reduce(reducer, 0);
 
-// Code your solution here
+module.exports = totalBatteries;
